015ce60aa9ef1bac56da57f8c53238829c0d69fa
bobczanki <bobczanki@gmail.com>
2014-09-07 17:47:29 +0200

    Fizz God mode v0.5

--- Fizz God mode.lua
+++ Fizz God mode.lua
@@ -1,8 +1,6 @@
-local version = "0.6"
+local version = "0.5"
 
 if myHero.charName ~= "Fizz" then return end
-
-AUTOUPDATE = true
 
 local lib_Required = {
 	["SOW"]			= "https://raw.githubusercontent.com/Hellsing/BoL/master/common/SOW.lua",
@@ -35,27 +33,27 @@ if lib_downloadNeeded then return end
 
 
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/bobczanki/B0L/edit/master/Fizz%20God%20mode.lua".."?rand="..math.random(1,10000)
+local UPDATE_PATH = "/bobczanki/B0L/master/Fizz%20God%20mode.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = SCRIPT_PATH .. GetCurrentEnv().FILE_NAME
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
-function _AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Fizz God mode:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
-if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/bobczanki/B0L/master/Fizz%20God%20mode.version")
-	if ServerData then
-		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
-		if ServerVersion then
-			if tonumber(version) < ServerVersion then
-				_AutoupdaterMsg("New version available ("..ServerVersion..")")
-				_AutoupdaterMsg("Updating, please don't press F9")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
-			else
-				_AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
-			end
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Fizz God mode:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+
+local ServerData = GetWebResult(UPDATE_HOST, "/bobczanki/B0L/master/Fizz%20God%20mode.version")
+
+if ServerData then
+	ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+	if ServerVersion then
+		if tonumber(version) < ServerVersion then
+			AutoupdaterMsg("New version available ("..ServerVersion..")")
+			AutoupdaterMsg("Updating, please don't press F9")
+			DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+		else
+			AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
 		end
-	else
-		_AutoupdaterMsg("Error downloading version info")
 	end
+else
+	AutoupdaterMsg("Error downloading version info")
 end
 
 
